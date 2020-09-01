617587945e60386599f98d7584f4fdb42666366e
Paul Franco <mail@paulfran.co>
2020-09-01 04:29:10 -0700

    Initial Commit

---
+++ trippy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trippy
+//
+//  Created by Paul Franco on 9/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
