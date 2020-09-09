4ad5e04940ca0706c33b1e09f374aaa4717b165b
Paul Franco <mail@paulfran.co>
2020-09-09 09:05:00 -0700

    setup gallery in the Vacation Details

---
+++ trippy/Cells/ThumbnailCell.swift
@@ -0,0 +1,22 @@
+//
+//  ThumbnailCell.swift
+//  trippy
+//
+//  Created by Paul Franco on 9/9/20.
+//
+
+import UIKit
+import SDWebImage
+
+class ThumbnailCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    func configureCell(url: String) {
+        img.layer.cornerRadius = 5
+        guard let url = URL(string: url) else { return }
+        img.sd_imageIndicator = SDWebImageActivityIndicator.medium
+        img.sd_setImage(with: url, placeholderImage: UIImage(named: "background-beach-alpha"))
+    }
+    
+}
--- trippy/VacationDetailsVC.swift
+++ trippy/VacationDetailsVC.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import SDWebImage
 
 class VacationDetailsVC: UIViewController {
     
     @IBOutlet weak var activitiesLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var numberOfNights: UILabel!
+    @IBOutlet weak var airfareLabel: UILabel!
+    
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var vacation: Vacation!
 
@@ -20,6 +27,21 @@ class VacationDetailsVC: UIViewController {
         title = vacation.title
         activitiesLabel.text = vacation.activities
         descriptionLabel.text = vacation.description
+        
+        priceLabel.text = "All inclusive price: " + vacation.price.formatToCurrencyString()
+        numberOfNights.text = "\(vacation.numberOfNights) night accomodations"
+        airfareLabel.text = vacation.airfare
+        
+        
+        let imageUrl = vacation.images[0]
+        if let url = URL(string: imageUrl) {
+            mainImage.layer.cornerRadius = 10
+            mainImage.sd_imageIndicator = SDWebImageActivityIndicator.medium
+            mainImage.sd_setImage(with: url, placeholderImage: UIImage(named: "background-beach-alpha"))
+        }
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
 
@@ -33,4 +55,30 @@ class VacationDetailsVC: UIViewController {
     }
     */
 
+}
+
+extension VacationDetailsVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return vacation.images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ThumbnailCell", for: indexPath) as! ThumbnailCell
+        let url = vacation.images[indexPath.row]
+        cell.configureCell(url: url)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let imageUrl = vacation.images[indexPath.item]
+        if let url = URL(string: imageUrl) {
+            mainImage.layer.cornerRadius = 10
+            mainImage.sd_imageIndicator = SDWebImageActivityIndicator.medium
+            mainImage.sd_setImage(with: url, placeholderImage: UIImage(named: "background-beach-alpha"))
+        }    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 120, height: 120)
+    }
+    
 }
